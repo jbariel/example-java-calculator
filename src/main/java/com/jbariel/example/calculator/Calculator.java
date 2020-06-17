3febc1cb27983fd6a587e7b272d6a1a0c389c799
Jarrett Bariel <jbariel@codewhispereronsulting.com>
2020-06-17 07:32:17 +0000

    hello world works

---
+++ src/main/java/com/jbariel/example/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.jbariel.example.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Calculator {
+
+    public static final Logger LOG = LoggerFactory.getLogger(Calculator.class);
+
+    public static void main(String[] args) {
+        LOG.info("Hello World");
+        System.exit(0);
+    }
+
+}
---
+++ src/test/java/com/jbariel/example/calculator/CalculatorTest.java
@@ -0,0 +1,5 @@
+package com.jbariel.example.calculator;
+
+public class CalculatorTest {
+
+}
