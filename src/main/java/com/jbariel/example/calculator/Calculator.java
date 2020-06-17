feda2f02bcb17610b2799a0668bb7df83f3dd7c1
Jarrett Bariel <jbariel@codewhispereronsulting.com>
2020-06-17 08:05:32 +0000

    we have actors working!

--- src/main/java/com/jbariel/example/calculator/Calculator.java
+++ src/main/java/com/jbariel/example/calculator/Calculator.java
@@ -14,6 +14,7 @@ public class Calculator {
 
     public static void main(final String[] args) {
         List<String> argStrs = Arrays.asList(args);
+        LOG.info("Performing " + argStrs.stream().collect(Collectors.joining(" ")));
         if (argStrs.size() < 3) {
             IllegalArgumentException e = new IllegalArgumentException(
                     "Must provide at least three (3) arguments: an actor and at least 2 digits");
@@ -21,7 +22,32 @@ public class Calculator {
             LOG.error(e.getLocalizedMessage(), e);
             throw e;
         }
-        LOG.info("Performing " + argStrs.stream().collect(Collectors.joining(" ")));
+        doAction(argStrs);
+    }
+
+    protected static void doAction(final List<String> args) {
+        String actor = args.get(0);
+        switch (actor.toLowerCase()) {
+            case "+":
+            case "add":
+                LOG.info("We're gonna add...");
+                break;
+            case "-":
+            case "subtract":
+                LOG.info("We're gonna subtract...");
+                break;
+            case "*":
+            case "multiply":
+                LOG.info("We're gonna multiply...");
+                break;
+            case "/":
+            case "divide":
+                LOG.info("We're gonna divide...");
+                break;
+            default:
+                throw new IllegalArgumentException(
+                        "Must provide a supported actor: ['+','-','*','/','add','subtract','multiply','divide'");
+        }
     }
 
 }
--- src/test/java/com/jbariel/example/calculator/CalculatorTest.java
+++ src/test/java/com/jbariel/example/calculator/CalculatorTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 
     @Test
-    @Disabled
     public void testLogArgs() {
         Calculator.main(new String[] { "*", "1", "2", "3" });
         // check logs to validate that "Peforming a b c" is output as info
@@ -17,9 +16,31 @@ public class CalculatorTest {
     @Test
     public void testHasThreeArgs() {
         shouldThrowIllegalArgumentException(new String[] {});
-        shouldThrowIllegalArgumentException(new String[] { "foo" });
-        shouldThrowIllegalArgumentException(new String[] { "foo", "bar" });
-        shouldNotThrowIllegalArgumentException(new String[] { "foo", "bar", "bar" });
+        shouldThrowIllegalArgumentException(new String[] { "+" });
+        shouldThrowIllegalArgumentException(new String[] { "+", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "+", "1", "1" });
+    }
+
+    @Test
+    public void testActorSymbolArg() {
+        shouldThrowIllegalArgumentException(new String[] { "%", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "+", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "-", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "*", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "/", "1", "1" });
+    }
+
+    @Test
+    public void testActorWordArg() {
+        shouldThrowIllegalArgumentException(new String[] { "modulus", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "add", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "ADD", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "subtract", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "SUBTRACT", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "multiply", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "MULTIPLY", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "divide", "1", "1" });
+        shouldNotThrowIllegalArgumentException(new String[] { "DIVIDE", "1", "1" });
     }
 
     protected void shouldThrowIllegalArgumentException(String[] args) {
