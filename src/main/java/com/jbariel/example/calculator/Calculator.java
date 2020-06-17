91ff7538ac443a9a54952eb8a7eaced6678bc274
Jarrett Bariel <jbariel@codewhispereronsulting.com>
2020-06-17 07:57:14 +0000

    start to build out arg tests

--- src/main/java/com/jbariel/example/calculator/Calculator.java
+++ src/main/java/com/jbariel/example/calculator/Calculator.java
@@ -1,5 +1,10 @@
 package com.jbariel.example.calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,9 +12,16 @@ public class Calculator {
 
     public static final Logger LOG = LoggerFactory.getLogger(Calculator.class);
 
-    public static void main(String[] args) {
-        LOG.info("Hello World");
-        System.exit(0);
+    public static void main(final String[] args) {
+        List<String> argStrs = Arrays.asList(args);
+        if (argStrs.size() < 3) {
+            IllegalArgumentException e = new IllegalArgumentException(
+                    "Must provide at least three (3) arguments: an actor and at least 2 digits");
+            LOG.debug("Read: " + argStrs.stream().collect(Collectors.joining(" ")));
+            LOG.error(e.getLocalizedMessage(), e);
+            throw e;
+        }
+        LOG.info("Performing " + argStrs.stream().collect(Collectors.joining(" ")));
     }
 
 }
--- src/test/java/com/jbariel/example/calculator/CalculatorTest.java
+++ src/test/java/com/jbariel/example/calculator/CalculatorTest.java
@@ -1,5 +1,41 @@
 package com.jbariel.example.calculator;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
 
+    @Test
+    @Disabled
+    public void testLogArgs() {
+        Calculator.main(new String[] { "*", "1", "2", "3" });
+        // check logs to validate that "Peforming a b c" is output as info
+    }
+
+    @Test
+    public void testHasThreeArgs() {
+        shouldThrowIllegalArgumentException(new String[] {});
+        shouldThrowIllegalArgumentException(new String[] { "foo" });
+        shouldThrowIllegalArgumentException(new String[] { "foo", "bar" });
+        shouldNotThrowIllegalArgumentException(new String[] { "foo", "bar", "bar" });
+    }
+
+    protected void shouldThrowIllegalArgumentException(String[] args) {
+        try {
+            Calculator.main(args);
+            fail("Should throw exception");
+        } catch (IllegalArgumentException e) {
+            // ignore
+        }
+    }
+
+    protected void shouldNotThrowIllegalArgumentException(String[] args) {
+        try {
+            Calculator.main(args);
+        } catch (IllegalArgumentException e) {
+            fail("Should not throw exception");
+        }
+    }
 }
